0a54a290ad09f65120d35def25f6f2e929d9113e
MILA AMELIA <102271236+milaaaamelia@users.noreply.github.com>
2023-09-18 12:01:15 +0700

    Add files via upload

--- lib/ui/form_data.dart
+++ lib/ui/form_data.dart
@@ -1,20 +1,78 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-class FormData extends StatefulWidget {
-  const FormData({Key? key}) : super(key: key);
+// ui/form_data.dart
+
+import 'package:flutter/material.dart';
+import 'package:tugasprakmob/ui/tampil_data.dart';
+
+class FormDataPage extends StatefulWidget {
+  const FormDataPage({super.key});
 
   @override
-  _FormDataState createState() => _FormDataState();
+  // ignore: library_private_types_in_public_api
+  _FormDataPageState createState() => _FormDataPageState();
 }
 
-class _FormDataState extends State<FormData> {
+class _FormDataPageState extends State<FormDataPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _nimController = TextEditingController();
+  final TextEditingController _birthYearController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Input Data"),
+        title: const Text('Input Data'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: 'Nama'),
+            ),
+            TextField(
+              controller: _nimController,
+              decoration: const InputDecoration(labelText: 'NIM'),
+            ),
+            TextField(
+              controller: _birthYearController,
+              decoration: const InputDecoration(labelText: 'Tahun Lahir'),
+              keyboardType: TextInputType.number,
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Validasi data atau lakukan tindakan lainnya
+                String name = _nameController.text;
+                String nim = _nimController.text;
+                String birthYear = _birthYearController.text;
+
+                // Pindah ke halaman tampil_data.dart dengan membawa data
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => TampilDataPage(
+                      name: name,
+                      nim: nim,
+                      birthYear: birthYear,
+                    ),
+                  ),
+                );
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _nimController.dispose();
+    _birthYearController.dispose();
+    super.dispose();
   }
 }
--- lib/ui/tampil_data.dart
+++ lib/ui/tampil_data.dart
@@ -1,15 +1,47 @@
+// ui/tampil_data.dart
+
 import 'package:flutter/material.dart';
 
-class TampilData extends StatelessWidget {
-  final String data; //contoh data yang di-passing
-  
-  const TampilData({Key? key, required this.data}) : super(key: key);
+class TampilDataPage extends StatelessWidget {
+  final String name;
+  final String nim;
+  final String birthYear;
+
+  const TampilDataPage({
+    super.key,
+    required this.name,
+    required this.nim,
+    required this.birthYear,
+  });
+
+  int calculateAge() {
+    int currentYear = DateTime.now().year;
+    return currentYear - int.parse(birthYear);
+  }
 
   @override
   Widget build(BuildContext context) {
+    int age = calculateAge();
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Perkenalan"),
+        title: const Text('Perkenalan'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Nama: $name', style: const TextStyle(fontSize: 18.0)),
+            Text('NIM: $nim', style: const TextStyle(fontSize: 18.0)),
+            Text('Usia: $age tahun', style: const TextStyle(fontSize: 18.0)),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context); // Kembali ke tampilan form
+              },
+              child: const Text('Kembali ke Form'),
+            ),
+          ],
+        ),
       ),
     );
   }
